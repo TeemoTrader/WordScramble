2eaa1fc44dfe6d840da5e681344edcc0333dded9
TeemoTrader <austinnorman1995@gmail.com>
2021-07-14 19:00:23 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Teemo Norman on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
